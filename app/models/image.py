8350b653cc725ca011bc94d4feff727a71ee830c
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-18 14:33:18 -0700

    created a model for images

---
+++ app/models/image.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    listing_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('listings.id')))
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    image = db.Column(db.String, nullable=False)
+    is_display_image = db.Column(db.Boolean, default=False)
+
+    # * Relationships 💚
+    # One to Many
+ 
+    # Many to Many
