6db8ca0d5f4a17519dcc4ad79b91ba33229290d7
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-19 09:27:30 -0700

    wrote a to_dict for the listing model

--- app/api/listing_routes.py
+++ app/api/listing_routes.py
@@ -7,4 +7,28 @@ from flask_wtf.csrf import CSRFProtect, generate_csrf
 
 listing_routes = Blueprint('listings', __name__)
 
+#* ------------------------         FULL CRUD          --------------------------
+
+
+# * -----------  GET  --------------
+#  Returns all the listings
+
+@listing_routes.route("")
+def get_listings():
+    listings = Listing.query.all()
+    return listings.to_dict_simple()
+
+
+# * -----------  GET  --------------
+# Returns a single listing by its id 
+
+@listing_routes.route('/<int:id>')
+def get_listing_by_id(id):
+    listing = Listing.query.get(id)
+    if not listing:
+        return {
+            "message": "Listing could not be found",
+            "status_code": 404
+        }, 404
+    return listing.to_dict()
 
