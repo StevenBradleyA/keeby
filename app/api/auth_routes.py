a77428494099bcf6352e9bd96d7ff46393717395
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-19 11:40:32 -0700

    sign up form and to dict refactor

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -65,7 +65,13 @@ def sign_up():
         user = User(
             username=form.data['username'],
             email=form.data['email'],
-            password=form.data['password']
+            password=form.data['password'],
+            first_name=form.data['first_name'],
+            last_name=form.data['last_name'],
+            profile_picture=form.data['profile_picture'],
+            daily_driver=form.data['daily_driver'],
+            keycaps=form.data['keycaps'],
+            switches=form.data['switches']
         )
         db.session.add(user)
         db.session.commit()
