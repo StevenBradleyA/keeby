dde70927998f39958f13cfb7cb8c7507a16b1f1e
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-19 12:48:03 -0700

    created new actions for create update delete

--- react-app/src/store/listing.js
+++ react-app/src/store/listing.js
@@ -1,9 +1,24 @@
-const LOAD_LISTINGS = "listings/LOAD_LISTINGS"
-const CREATE_LISTING = "listings/CREATE_LISTING"
-const UPDATE_LISTING = "listings/UPDATE_LISTING"
-const DELETE_LISTING = "listings/DELETE_LISTING"
+const LOAD_LISTINGS = "listings/LOAD_LISTINGS";
+const CREATE_LISTING = "listings/CREATE_LISTING";
+const UPDATE_LISTING = "listings/UPDATE_LISTING";
+const DELETE_LISTING = "listings/DELETE_LISTING";
 
 const loadListings = (allListingData) => ({
-    type: LOAD_LISTINGS,
-    payload: allListingData,
-  });
+  type: LOAD_LISTINGS,
+  payload: allListingData,
+});
+
+const createListing = (newListingData) => ({
+  type: CREATE_LISTING,
+  payload: newListingData,
+});
+
+const updateListing = (updatedListingData) => ({
+  type: UPDATE_LISTING,
+  payload: updatedListingData,
+});
+
+const deleteListing = (listingId) => ({
+  type: DELETE_LISTING,
+  payload: listingId,
+});
