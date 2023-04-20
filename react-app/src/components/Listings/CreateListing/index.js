ec423c6ea13465674df05d6f3cf2e09a87562c91
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-20 16:26:59 -0700

    created new input for accepting multiple files

--- react-app/src/components/Listings/CreateListing/index.js
+++ react-app/src/components/Listings/CreateListing/index.js
@@ -9,7 +9,7 @@ function CreateListingForm({ workspaceId }) {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
+  const [images, setImages] = useState([]);
 
   const [errors, setErrors] = useState({});
   const [hasSubmitted, setHasSubmitted] = useState(false);
@@ -26,13 +26,38 @@ function CreateListingForm({ workspaceId }) {
       errorsObj.description =
         "Your Description must be at least 400 characters";
     }
-    if (image.length < 4) {
-      errorsObj.image = "Provide at least 4 Photos";
-    }
-    if (image.length > 50) {
-      errorsObj.imageExcess = "Cannot provide more than 50 photos";
-    }
+    // if (image.length < 4) {
+    //   errorsObj.image = "Provide at least 4 Photos";
+    // }
+    // if (image.length > 50) {
+    //   errorsObj.imageExcess = "Cannot provide more than 50 photos";
+    // }
     setErrors(errorsObj);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
+
+    // aws uploads can be a bit slow—displaying
+    // some sort of loading message is a good idea
+    setImageLoading(true);
+
+    const res = await fetch("/api/images", {
+      method: "POST",
+      body: formData,
+    });
+    if (res.ok) {
+      await res.json();
+      setImageLoading(false);
+      history.push("/images");
+    } else {
+      setImageLoading(false);
+      // a real app would probably use more advanced
+      // error handling
+      console.log("error");
+    }
   };
 
   useEffect(() => {
@@ -44,18 +69,14 @@ function CreateListingForm({ workspaceId }) {
 
     if (!Object.values(errors).length) {
       const listingInformation = {
+        owner_id: sessionUser.id,
         name,
         price,
         description,
         image,
-
-        workspace_id: Number(workspaceId),
-        is_channel: true,
       };
       let newListing = await dispatch(createListingThunk(listingInformation));
-      // let newChannel = await dispatch(createChannelThunk(channelInformation));
-      dispatch(refreshUser(sessionUser.id));
-
+      //   push to new Listing id
       history.push(`/`);
     }
     setHasSubmitted(true);
@@ -103,19 +124,36 @@ function CreateListingForm({ workspaceId }) {
                 Description:
                 <input
                   type="text"
-                  placeholder="Description"
+                  placeholder="Must be greater than 400 characters"
                   value={description}
                   onChange={(e) => setDescription(e.target.value)}
                 />
               </label>
               <p></p>
-              {hasSubmitted && errors.price && (
-                <p className="errors">{errors.price}</p>
+              {hasSubmitted && errors.description && (
+                <p className="errors">{errors.description}</p>
               )}
+              <p>Upload Images of your keyboard!</p>
+              <label>
+                Upload at least 4 images:
+                <input
+                  type="file"
+                  multiple
+                  value={images}
+                  accept="image/png, image/jpeg"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </label>
+              <p></p>
+              {/* {hasSubmitted && errors.image && (
+                <p className="errors">{errors.image}</p>
+              )}
+              {hasSubmitted && errors.imageExcess && (
+                <p className="errors">{errors.imageExcess}</p>
+              )} */}
               <input
-                className="create-channel-button"
                 type="submit"
-                value={"Create Channel"}
+                value={"Create Listing"}
                 disabled={hasSubmitted && Object.values(errors).length > 0}
               />
             </form>
