2bbb84663882cdfbaff9029f61c8f561dac13a12
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-23 15:53:25 -0700

    added images to an individual listing page

--- react-app/src/components/ListingPage/index.js
+++ react-app/src/components/ListingPage/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { getListingByIdThunk } from "../../store/listing";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import "./ListingPage.css"
+import "./ListingPage.css";
 
 const ListingPage = () => {
   const dispatch = useDispatch();
@@ -21,13 +21,51 @@ const ListingPage = () => {
     return <h1>LOADING...</h1>;
   }
 
-
-
+  // console.log('hey current', currentListing.listing_images[0].image)
+  const displayImageArr = currentListing.listing_images.filter(
+    (e) => e.is_display_image === true
+  );
+  // `${listing.description.slice(0, 350)}...`
   return (
     <div className="single-listing-page-container">
       <div>{currentListing.name}</div>
       <div>{currentListing.price}</div>
-      <div>{currentListing.description}</div>
+      <img
+        className="listing-page-display-image"
+        alt="display"
+        src={displayImageArr[0].image}
+      />
+      <div>{`${currentListing.description.slice(0, 250)}...`}</div>
+      <img
+        className="listing-page-additional-image"
+        alt="listing"
+        src={currentListing.listing_images[1].image}
+      />
+
+      <div>{`${currentListing.description.slice(250, 500)}...`}</div>
+      <img
+        className="listing-page-additional-image"
+        alt="listing"
+        src={currentListing.listing_images[2].image}
+      />
+
+      {/* <div>{`${currentListing.description.slice(200, 300)}...`}</div> */}
+      {/* <div>{`${currentListing.description.slice(300, 400)}...`}</div> */}
+      <div>{`${currentListing.description.slice(500, 750)}...`}</div>
+      <img
+        className="listing-page-additional-image"
+        alt="listing"
+        src={currentListing.listing_images[3].image}
+      />
+
+      <div>{`${currentListing.description.slice(750)}`}</div>
+
+      <h2>{`Photo Gallery`}</h2>
+      <div className="listing-page-photo-gallery">
+        {currentListing.listing_images.map((image) => (
+          <img alt="all listing" src={image.image} />
+        ))}
+      </div>
     </div>
   );
 };
