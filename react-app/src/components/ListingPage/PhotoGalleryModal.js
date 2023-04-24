12f23d8766263b0e479e3ea71f80e816f22bd981
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-23 17:16:12 -0700

    created a modal for photo gallery allowing the user to preview an image

---
+++ react-app/src/components/ListingPage/PhotoGalleryModal.js
@@ -0,0 +1,15 @@
+import React  from "react";
+
+
+function PhotoGalleryModal({ image }) {
+  
+
+
+  return (<div>
+
+    <img className="gallery-preview-modal" alt="gallery-preview" src={image.image}/>
+
+  </div>);
+}
+
+export default PhotoGalleryModal;
--- react-app/src/components/ListingPage/index.js
+++ react-app/src/components/ListingPage/index.js
@@ -2,30 +2,37 @@ import React, { useEffect } from "react";
 import { getListingByIdThunk } from "../../store/listing";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { useModal } from "../../context/Modal";
 import "./ListingPage.css";
+import PhotoGalleryModal from "./PhotoGalleryModal";
 
 const ListingPage = () => {
   const dispatch = useDispatch();
   const { listingId } = useParams();
-
+  const { setModalContent } = useModal();
   useEffect(() => {
     dispatch(getListingByIdThunk(listingId));
   }, [dispatch, listingId]);
 
   const currentListing = useSelector((state) => state.listings)[listingId];
 
-  //   going to need all comment info
-  // going to need all photos here
-  // everything doe
   if (!currentListing) {
     return <h1>LOADING...</h1>;
   }
 
-  // console.log('hey current', currentListing.listing_images[0].image)
   const displayImageArr = currentListing.listing_images.filter(
     (e) => e.is_display_image === true
   );
-  // `${listing.description.slice(0, 350)}...`
+
+  // todo
+  // make a modal for each preview image. the modal is just the image on click
+
+  const handlePhotoGalleryClick = (image) => {
+    return () => {
+      setModalContent(<PhotoGalleryModal image={image} />);
+    };
+  };
+
   return (
     <div className="single-listing-page-container">
       <div>{currentListing.name}</div>
@@ -35,34 +42,49 @@ const ListingPage = () => {
         alt="display"
         src={displayImageArr[0].image}
       />
-      <div className="listing-page-description">{`${currentListing.description.slice(0, 250)}...`}</div>
+      <div className="listing-page-description">{`${currentListing.description.slice(
+        0,
+        250
+      )}...`}</div>
       <img
         className="listing-page-additional-image"
         alt="listing"
         src={currentListing.listing_images[1].image}
       />
 
-      <div className="listing-page-description">{`${currentListing.description.slice(250, 500)}...`}</div>
+      <div className="listing-page-description">{`${currentListing.description.slice(
+        250,
+        500
+      )}...`}</div>
       <img
         className="listing-page-additional-image"
         alt="listing"
         src={currentListing.listing_images[2].image}
       />
 
-
-      <div className="listing-page-description">{`${currentListing.description.slice(500, 750)}...`}</div>
+      <div className="listing-page-description">{`${currentListing.description.slice(
+        500,
+        750
+      )}...`}</div>
       <img
         className="listing-page-additional-image"
         alt="listing"
         src={currentListing.listing_images[3].image}
       />
 
-      <div className="listing-page-description">{`${currentListing.description.slice(750)}`}</div>
+      <div className="listing-page-description">{`${currentListing.description.slice(
+        750
+      )}`}</div>
 
-      <h2>{`Photo Gallery`}</h2>
-      <div className="listing-page-photo-gallery">
+      <h2 className="listing-page-photo-gallery-title">{`Photo Gallery`}</h2>
+      <div className="listing-page-photo-gallery-container">
         {currentListing.listing_images.map((image) => (
-          <img alt="all listing" src={image.image} />
+          <img
+            className="listing-page-photo-gallery-each"
+            alt="all listing"
+            src={image.image}
+            onClick={handlePhotoGalleryClick(image)}
+          />
         ))}
       </div>
     </div>
