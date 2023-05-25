9d68b2ab8964bab1e00d8fd70cffbeeb4b507113
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-05-25 13:51:46 -0700

    changed search result styling to match new nav bar styling

--- react-app/src/components/Listings/SearchListing/index.js
+++ react-app/src/components/Listings/SearchListing/index.js
@@ -1,25 +1,35 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import "./SearchListing.css"
+import "./SearchListing.css";
 
-function ListingSearchResults({listing, setSearchResult, setName}) {
-const dispatch = useDispatch()
-const history = useHistory()
-    const handleListingClick = (e) => {
-        e.preventDefault()
-        history.push(`/listing/${listing.id}`)
-        setSearchResult('')
-        setName('')
-    }
+function ListingSearchResults({ listing, setSearchResult, setName }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleListingClick = (e) => {
+    e.preventDefault();
+    history.push(`/listing/${listing.id}`);
+    setSearchResult("");
+    setName("");
+  };
 
-  const previewImage =  listing.listing_images.filter((e)=> {
-        return e.is_display_image === true
-    })
+  const previewImage = listing.listing_images.filter((e) => {
+    return e.is_display_image === true;
+  });
   return (
-    <div onClick={handleListingClick} className="search-result-listing-card-container">
-        <img src={previewImage[0].image} className="search-result-listing-image"/>
+    <div
+      onClick={handleListingClick}
+      className="search-result-listing-card-container"
+    >
+      <div className="search-result-image-container">
+        <img
+          src={previewImage[0].image}
+          className="search-result-listing-image"
+        />
+      </div>
+      <div className="search-result-text-container">
         <div className="search-result-listing-name">{listing.name}</div>
+      </div>
     </div>
   );
 }
